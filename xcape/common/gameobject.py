968b54ca7e9e0040618144b3dac027719149cb44
Othman <oz.alikhan@gmail.com>
2017-05-30 14:22:59 +0100

    Added gameobject module to reduce duplicate docstrings

---
+++ xcape/common/gameobject.py
@@ -0,0 +1,31 @@
+"""
+The main purpose of this module is to reduce duplicate documentation by
+inheriting docstrings.
+"""
+
+
+class GameObject:
+    """
+    The interface for every class throughout the project.
+    """
+
+    def handleEvent(self, event):
+        """
+        Handles the given event.
+
+        :param event: pygame.Event, allowing event-driven programming.
+        """
+        pass
+
+    def update(self):
+        """
+        Updates the logic of the game object every game tick.
+        """
+        pass
+
+    def draw(self):
+        """
+        Renders the game object to the screen every game tick.
+        """
+        pass
+
