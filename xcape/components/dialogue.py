e5e2ec49da6ca55dae2ad6c3478f7044e43ca93a
Othman <oz.alikhan@gmail.com>
2017-06-25 17:21:29 +0100

    Translated dialogues into Spanish

--- xcape/components/dialogue.py
+++ xcape/components/dialogue.py
@@ -3,19 +3,19 @@ Contains all the dialogue used in speech bubbles.
 """
 
 BOSS_0 = "Mmmm..."
-BOSS_1 = "What do lawyers wear in court? Law suits."
-BOSS_2 = "When is a door not a door? When it is ajar!"
-BOSS_3 = "Why do java programmers wear glasses? Because they don't C#"
-BOSS_4 = "What do you call a bee that lives in America? A USB."
+BOSS_1 = "Cambio Pentium por calculadora que sepa dividir."
+BOSS_2 = "PATH: la primera a la derecha y luego todo recto"
+BOSS_3 = "VVaayyaa tteeccllaaddoo mmaass sseennssiibbllee"
+BOSS_4 = "No perdí la cabeza... La copia de seguridad está en algún sitio."
 
 SCENE_SOLO_1 = "Capítulo 1: Es bastante acogedor aquí..."
 SCENE_SOLO_2 = "Capítulo 2: Y... esto es... la prision?"
 SCENE_SOLO_3 = "Capítulo 3: Quién siquiera necesita escapar... cierto?"
 
-SCENE_COOP_1A = "Chapter 1: I don't mind being buried next to you..."
-SCENE_COOP_1B = "HINT: Find the coop jump button on the keyboard!"
-SCENE_COOP_2 = "Chapter 2: Déjà vu... or not...?"
-SCENE_COOP_3 = "Chapter 3: Who let the... pigs... out...?"
+SCENE_COOP_1A = "Capítulo 1: No me importa ser enterrado junto a ti..."
+SCENE_COOP_1B = "PISTA: Encuentra el salto coop en el teclado!"
+SCENE_COOP_2 = "Capítulo 2: Déjà vu... o no...?"
+SCENE_COOP_3 = "Capítulo 3: Quien dejó salir... a los cerdos...?"
 
 OFFICE_1 = "Cuéntame qué necesitas."
 OFFICE_2 = "Necesito tus servicios como asesino."
